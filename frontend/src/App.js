9c958a33cfcdec4287b3ac2735435ef701b96301
Musthafa Kariyaparambil <musthafakdml@gmail.com>
2020-06-18 18:44:47 +0530

    Made some changes to the App.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello there
+          This changes are made part of some-feature
         </p>
         <a
           className="App-link"
